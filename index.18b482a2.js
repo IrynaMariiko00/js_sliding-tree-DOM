11e876a24ba1961928c23ffc1669e88a64080edb
Iryna Mariiko <IrynaMM497@gmail.com>
2024-10-28 16:20:37 +0200

    make build

---
+++ index.18b482a2.js
@@ -0,0 +1,2 @@
+document.querySelector(".tree").addEventListener("click",function(e){if("LI"===e.target.tagName){var l=e.target.querySelector("ul");null!==l&&(""===l.style.display||"block"===l.style.display?l.style.display="none":l.style.display="block")}});
+//# sourceMappingURL=index.18b482a2.js.map
